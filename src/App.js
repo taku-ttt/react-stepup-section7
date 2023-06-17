d3cf820bcb7d121529855cab96d7a851f0695227
taku-ttt <135543613+taku-ttt@users.noreply.github.com>
2023-06-17 23:33:38 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { UserProvider } from "./providers/UserProvider";
+import { Router } from "./router/Router";
+import "./styles.css";
+import { RecoilRoot } from "recoil";
+
+export default function App() {
+  return (
+    <RecoilRoot>
+      <UserProvider>
+        <Router />;
+      </UserProvider>
+    </RecoilRoot>
+  );
+}
